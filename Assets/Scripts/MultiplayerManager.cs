6e4e3a6df1b42bdee2fa7ccb8c72e9ab2d91a202
MustacheLincoln <MustacheLincoln@gmail.com>
2021-08-02 16:09:52 -0500

    Added Join IP and Password Protection

--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -1,34 +1,114 @@
 using MLAPI;
 using UnityEngine;
+using TMPro;
+using System.Text;
+using MLAPI.Transports.UNET;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    void OnGUI()
-    {
-        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-        {
-            StartButtons();
-        }
-        else
-        {
-            StatusLabels();
-        }
+    [SerializeField] private TMP_InputField IPInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private GameObject entryUI;
+    [SerializeField] private GameObject leaveButton;
 
-        GUILayout.EndArea();
+    private void Start()
+    {
+        NetworkManager.Singleton.OnServerStarted += HandleServerStarted;
+        NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnect;
     }
 
-    static void StartButtons()
+    private void OnDestroy()
     {
-        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if(NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnServerStarted -= HandleServerStarted;
+            NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnect;
+        }
     }
 
-    static void StatusLabels()
+    public void Host()
     {
-        var mode = NetworkManager.Singleton.IsHost ?
-            "Host" : "Client";
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+        NetworkManager.Singleton.StartHost(new Vector3(0,0,-2), Quaternion.Euler(0,90,0));
+    }
 
-        GUILayout.Label("Mode: " + mode);
+    public void Join()
+    {
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = Encoding.ASCII.GetBytes(passwordInputField.text);
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public void Leave()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.StopHost();
+            NetworkManager.Singleton.ConnectionApprovalCallback -= ApprovalCheck;
+        }
+        else if (NetworkManager.Singleton.IsClient)
+        {
+            NetworkManager.Singleton.StopClient();
+        }
+
+        entryUI.SetActive(true);
+        leaveButton.SetActive(false);
+    }
+
+    private void HandleServerStarted()
+    {
+        HandleClientConnected(NetworkManager.Singleton.LocalClientId);
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        if(clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            entryUI.SetActive(false);
+            leaveButton.SetActive(true);
+        }
+    }
+
+    private void HandleClientDisconnect(ulong clientId)
+    {
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            entryUI.SetActive(true);
+            leaveButton.SetActive(false);
+        }
+    }
+
+    private void ApprovalCheck(byte[] connectionData, ulong clientId, NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        string password = Encoding.ASCII.GetString(connectionData);
+
+        bool approveConnection = password == passwordInputField.text;
+
+        NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = IPInputField.text;
+
+        //Client Spawn Position
+        Vector3 spawnPos = Vector3.zero;
+        Quaternion spawnRot = Quaternion.identity;
+
+        switch (NetworkManager.Singleton.ConnectedClients.Count)
+        {
+            case 1:
+                spawnPos = new Vector3(0, 0, 0);
+                spawnRot = Quaternion.Euler(0, 90, 0);
+                break;
+            case 2:
+                spawnPos = new Vector3(2, 0, 0);
+                spawnRot = Quaternion.Euler(0, 90, 0);
+                break;
+            case 3:
+                spawnPos = new Vector3(-2, 0, 0);
+                spawnRot = Quaternion.Euler(0, 90, 0);
+                break;
+        }
+
+
+        callback(true, null, approveConnection, spawnPos, spawnRot);
     }
 }
